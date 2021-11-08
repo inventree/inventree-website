8a98549ccc6e27b6b1653b5e8b8fadb4abdf0772
Matthias Mair <matmair@live.de>
2021-11-08 22:49:58 +0100

    change pipeline to use postcss

--- home/postcss.config.js
+++ home/postcss.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    plugins: [require("tailwindcss"), require("autoprefixer")],
+    plugins: [
+        require('postcss-import'),
+        require("tailwindcss"),
+        require("autoprefixer"),
+        require('cssnano')({
+            preset: 'default',
+        }),
+    ],
 }
