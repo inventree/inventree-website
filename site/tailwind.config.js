c4cee6168c2010e10430da7d88b749f03824279f
Matthias Mair <matmair@live.de>
2022-01-23 03:42:23 +0100

    update configs

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,19 +1,15 @@
-
 module.exports = {
-    purge: {
-        enabled: true,
-        content: ['./**/*.html'],
-      },
-    darkMode: 'media',
-    theme: {
-        extend: {
-            colors: {
-              'primary':'#90A8D8',
-              'secondary':'#DDBC91',
-            }
-        },
+  content: ["./**/*.html"],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#90A8D8',
+        'secondary':'#DDBC91',
+      }
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
