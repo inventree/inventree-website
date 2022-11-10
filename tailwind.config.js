40488fd5e9d9f08535d007acfd5c4878c87056fe
Matthias Mair <code@mjmair.com>
2022-11-10 21:15:02 +0000

    switch colors -> maybe better contrast on links?

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
     purge: {
         enabled: true,
         content: ['./**/*.html'],
-      },
+    },
     darkMode: 'media',
     theme: {
         extend: {
             colors: {
-              'primary':'#90A8D8',
-              'secondary':'#DDBC91',
+                'primary': '#DDBC91',
+                'secondary': '#90A8D8',
             },
             screens: {
                 'xs': '470px',
