f149e61244db5e8ebc34c40c7208c56a729a1270
Matthias Mair <code@mjmair.com>
2022-11-10 22:29:41 +0100

    Accessibility improvements (#53)
    * add alt tags to lear more
    
    * make logo section better screenreadable
    
    * enhance contrast on footer
    
    * add alt text to back button
    
    * switch colors -> maybe better contrast on links?
    
    * make link text better readable
    
    * switch color back
    
    * and another try
    
    * Revert "and another try"
    
    This reverts commit cb58c92c51877ce07771cb1a95ffb8d5e460beee.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
     purge: {
         enabled: true,
         content: ['./**/*.html'],
-      },
+    },
     darkMode: 'media',
     theme: {
         extend: {
             colors: {
-              'primary':'#90A8D8',
-              'secondary':'#DDBC91',
+                'primary': '#90A8D8',
+                'secondary': '#DDBC91',
             },
             screens: {
                 'xs': '470px',
