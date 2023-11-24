45a0e04883f15fbd0e0c84acbf415f4e95a402dd
Oliver <oliver.henry.walters@gmail.com>
2023-11-24 13:13:44 +1100

    Split project stats out into separate file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,9 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDocker } from '@fortawesome/free-brands-svg-icons';
-import { faCodeFork, faLanguage, faStar, faUsers } from '@fortawesome/free-solid-svg-icons';
+import ProjectStats from '@site/src/components/HomepageFeatures/ProjectStats';
 
-import { InvenTreeStats } from '@site/src/components/HomepageFeatures/stats';
+
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -35,41 +33,7 @@ function HomepageHeader() {
 }
 
 
-function ProjectStatsItem({icon, value, label}) {
 
-  return (
-    <div className="col col--4">
-      <div className="text--center">
-        <h3>
-          <FontAwesomeIcon icon={icon} size='xl' />
-          {value}
-        </h3>
-        <p>{label}</p>
-      </div>
-    </div>
-  );
-}
-
-
-function ProjectStats() {
-  return (
-    <section className={styles.projectStats}>
-      <div className="container">
-        <Heading as="h2">Harness the power of open source</Heading>
-        <p>
-          InvenTree is an open source project built by a community of developers.
-        </p>
-        <div className="row">
-          <ProjectStatsItem icon={faDocker} value={InvenTreeStats.docker_pulls} label="Docker Pulls" />
-          <ProjectStatsItem icon={faUsers} value={InvenTreeStats.github_contributors} label="Contributors" />
-          <ProjectStatsItem icon={faCodeFork} value={InvenTreeStats.github_forks} label="GitHub Forks" />
-          <ProjectStatsItem icon={faStar} value={InvenTreeStats.github_stars} label="GitHub Stars" />
-          <ProjectStatsItem icon={faLanguage} value={InvenTreeStats.crowdin_languages} label="Supported Languages" />
-        </div>
-      </div>
-    </section>
-  );
-}
 
 
 export default function Home(): JSX.Element {
