6aaec3d0acbd703ef2b41556d59e0f874d951eed
Oliver <oliver.henry.walters@gmail.com>
2023-11-24 12:08:13 +1100

    Add stats to front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { InvenTreeStats } from '@site/src/components/HomepageFeatures/stats';
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -28,15 +30,54 @@ function HomepageHeader() {
   );
 }
 
+
+function ProjectStatsItem({icon, value, label}) {
+
+  return (
+    <div className="col col--4">
+      <div className="text--center">
+        <h3>
+          <FontAwesomeIcon icon={icon} size='xl' />
+          {value}
+        </h3>
+        <p>{label}</p>
+      </div>
+    </div>
+  );
+}
+
+
+function ProjectStats() {
+  return (
+    <section className={styles.projectStats}>
+      <div className="container">
+        <Heading as="h2">Harness the power of open source</Heading>
+        <p>
+          InvenTree is an open source project built by a community of developers.
+        </p>
+        <div className="row">
+          <ProjectStatsItem icon={['fab', 'docker']} value={InvenTreeStats.docker_pulls} label="Docker Pulls" />
+          <ProjectStatsItem icon={['fas', 'users']} value={InvenTreeStats.github_contributors} label="Contributors" />
+          <ProjectStatsItem icon={['fas', 'fa-code-fork']} value={InvenTreeStats.github_forks} label="GitHub Forks" />
+          <ProjectStatsItem icon={['fas', 'fa-star']} value={InvenTreeStats.github_stars} label="GitHub Stars" />
+          <ProjectStatsItem icon={['fas', 'language']} value={InvenTreeStats.crowdin_languages} label="Supported Languages" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="InvenTree - Intuitive Inventory Management">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
+        <ProjectStats />
       </main>
     </Layout>
   );
