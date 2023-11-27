2e80900f3fe2f0f76a7ff6c779b37113ce20dc42
Oliver <oliver.henry.walters@gmail.com>
2023-11-28 10:57:24 +1100

    Throw error on broken markdown links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   projectName: 'inventree-website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
